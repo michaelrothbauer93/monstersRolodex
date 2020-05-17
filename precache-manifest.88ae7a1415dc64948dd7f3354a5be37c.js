a783206ae00802b0aa0d61dafe83d8d8a2221106
Michael Rothbauer <mrothbauer@Michaels-MacBook-Pro.local>
2020-05-17 17:57:51 -0500

    Updates

---
+++ precache-manifest.88ae7a1415dc64948dd7f3354a5be37c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f5003adb1ca1f6e5fbcbc20195afcdc",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "49c115166905a342812a",
+    "url": "/reactapp/static/css/main.0d19469b.chunk.css"
+  },
+  {
+    "revision": "95bef5d1f0b8c893b684",
+    "url": "/reactapp/static/js/2.9818ad5f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.9818ad5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c115166905a342812a",
+    "url": "/reactapp/static/js/main.ae751021.chunk.js"
+  },
+  {
+    "revision": "ce941aac6d59ccdf0c6d",
+    "url": "/reactapp/static/js/runtime-main.c373a6aa.js"
+  }
+]);
